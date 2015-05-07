30123dc4019b8c009a4be16d88c1b8d02f6b33ea
Eric Sukumar <esukumar@drwholdings.com>
2015-05-07 10:45:56 -0500

    final 20150507v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,8 @@ require 'date'
 Event.delete_all
 Note.delete_all
 Event.create(title:'Chicago Cubs Game', picture:'http://lakeshorepublicmedia.org/wp-content/uploads/2013/07/Wrigley.jpg')
+Event.create(title:'San Francisco Trip', picture:'http://www.babson.edu/Academics/undergraduate/academic-programs/PublishingImages/san-franc.jpg')
+Event.create(title:'Croatia Backpacking', picture:'http://www.lonelyplanet.com/maps/europe/croatia/map_of_croatia.jpg')
 # Note.create(title:'Sears Tower', time:DateTime.now, desc:'It was a great view from the top')
 puts "#{Event.count} Seeded succesfully."
 # This file should contain all the record creation needed to seed the database with its default values.
