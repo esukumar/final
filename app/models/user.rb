22c72e8ea1ca52630a3cb1a663b4bdf83176cf60
Eric Sukumar <esukumar@drwholdings.com>
2015-05-21 11:06:23 -0500

    final 20150521v1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   has_many :events
   has_many :comments
+  validates_presence_of :email, :password, :name
+  validates :email, length: {maximum: 30}, uniqueness: true
+  validates_format_of :email, :with => /@/
+  validates :password, length: {maximum: 30}
 end
