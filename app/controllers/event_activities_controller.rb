d699d4e41b10f1d71ae116560ad476167bd9b40a
Eric Sukumar <ericsukumar@gmail.com>
2015-05-27 09:46:27 -0500

    final 20150527v1

--- app/controllers/event_activities_controller.rb
+++ app/controllers/event_activities_controller.rb
@@ -10,13 +10,13 @@ class EventActivitiesController < ApplicationController
   def destroy
     @event_activity=EventActivity.find_by(id: params[:id])
     @event=Event.find_by(id:@event_activity.event_id)
-    activity_id = @event_activity.activity_id
-    @event_activity.delete
-    activity_count=EventActivity.where(activity_id:@event_activity.activity_id).count()
-    if activity_count==0
-      @activity=Activity.find_by(id:activity_id)
-      @activity.delete
-    end
+    # activity_id = @event_activity.activity_id
+    @event_activity.destroy
+    # activity_count=EventActivity.where(activity_id:@event_activity.activity_id).count()
+    # if activity_count==0
+    #   @activity=Activity.find_by(id:activity_id)
+    #   @activity.delete
+    # end
     redirect_to event_url(@event)
   end
 end
