3d6ca159fa96207134b7252ffbc290363f2aa595
Eric Sukumar <ericsukumar@gmail.com>
2015-06-01 23:33:56 -0500

    final 20150601v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,11 +13,25 @@ User.create(email: 'root@example.com', password: 'foo', name:'root')
 Event.create(title:'Chicago Cubs Game', picture:'http://lakeshorepublicmedia.org/wp-content/uploads/2013/07/Wrigley.jpg', user_id:User.find_by(email:'root@example.com').id)
 Event.create(title:'San Francisco Trip', picture:'http://www.babson.edu/Academics/undergraduate/academic-programs/PublishingImages/san-franc.jpg', user_id:User.find_by(email:'root@example.com').id)
 Event.create(title:'Croatia Backpacking', picture:'http://www.lonelyplanet.com/maps/europe/croatia/map_of_croatia.jpg', user_id:User.find_by(email:'root@example.com').id)
+Event.create(title:'Tomorrowland',picture:'http://fc03.deviantart.net/fs70/f/2014/333/5/2/tomorrowland_2_by_omerfk360-d8823q0.jpg', user_id:User.find_by(email:'root@example.com').id)
+Event.create(title:'Greek Islands',picture:'http://www.telegraph.co.uk/incoming/article72232.ece/ALTERNATES/w620/santorini.jpg', user_id:User.find_by(email:'root@example.com').id)
+Event.create(title:'London Visit',picture:'http://www.clinkhostels.com/wp-content/uploads/2015/03/london22.jpg', user_id:User.find_by(email:'root@example.com').id)
 
 Activity.create(title: 'Hiking')
+Activity.create(title: 'Spectating')
+Activity.create(title: 'Sunbathing')
+Activity.create(title: 'Snorkeling')
+Activity.create(title: 'Museums')
+Activity.create(title: 'Techno')
 
 EventActivity.create(event_id:Event.find_by(title:'San Francisco Trip').id,activity_id:Activity.find_by(title:'Hiking').id)
 EventActivity.create(event_id:Event.find_by(title:'Croatia Backpacking').id,activity_id:Activity.find_by(title:'Hiking').id)
+EventActivity.create(event_id:Event.find_by(title:'Chicago Cubs Game').id,activity_id:Activity.find_by(title:'Spectating').id)
+EventActivity.create(event_id:Event.find_by(title:'Greek Islands').id,activity_id:Activity.find_by(title:'Sunbathing').id)
+EventActivity.create(event_id:Event.find_by(title:'Croatia Backpacking').id,activity_id:Activity.find_by(title:'Snorkeling').id)
+EventActivity.create(event_id:Event.find_by(title:'Greek Islands').id,activity_id:Activity.find_by(title:'Snorkeling').id)
+EventActivity.create(event_id:Event.find_by(title:'London Visit').id,activity_id:Activity.find_by(title:'Museums').id)
+EventActivity.create(event_id:Event.find_by(title:'Tomorrowland').id,activity_id:Activity.find_by(title:'Techno').id)
 
 Note.create(title:'The 5th inning',desc:'The batter hit a home run and the crowd went wild!!',event_id:Event.find_by(title:'Chicago Cubs Game').id)
 Image.create(title: 'Home Base',picture:'http://wrigleyivy.com/wp-content/uploads/2013/02/wrigly_field_espn.jpg',event_id:Event.find_by(title:'Chicago Cubs Game').id)
@@ -28,6 +42,12 @@ Image.create(title:'Alcatraz Pic',picture:'http://upload.wikimedia.org/wikipedia
 
 Note.create(title:'Dubrovnik',desc:'This rustic old city was one of the major film sites for Game of Thrones',event_id:Event.find_by(title:'Croatia Backpacking').id)
 Note.create(title:'Hvar Island',desc:'Celebrities from around the world flock here for the nightlife',event_id:Event.find_by(title:'Croatia Backpacking').id)
+
+Note.create(title:'Santorini',desc:'There was a magnificent view from the rooftop restaurant',event_id:Event.find_by(title:'Greek Islands').id)
+
+Note.create(title:'Museum',desc:'We saw all of the crown jewels and medieval armor at the museum.',event_id:Event.find_by(title:'London Visit').id)
+
+Note.create(title:'DubStep',desc:'Our favorite artists dropped the bass at the dubstep stage.',event_id:Event.find_by(title:'Tomorrowland').id)
 
 puts "#{User.count} Users Seeded succesfully."
 puts "#{Event.count} Events Seeded succesfully."
